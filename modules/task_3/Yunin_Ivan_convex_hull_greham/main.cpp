6ca8070a89672659bf3ea0f992aedb90fcb058a3
IvanYunin <79103889939@yandex.ru>
2018-12-15 04:33:50 +0300

    only arrays

--- modules/task_3/Yunin_Ivan_convex_hull_greham/main.cpp
+++ modules/task_3/Yunin_Ivan_convex_hull_greham/main.cpp
@@ -66,13 +66,11 @@ int compare(const void *vp1, const void *vp2) {
 }
   
 // Finds upper tangent of two polygons 'a' and 'b' 
-// represented as two vectors. 
-vector<Point> merger(vector<Point > a, 
-                              vector<Point > b) 
-{ 
+// represented as two vectors. //rerurn size of vector
+int merger(Point *a, Point *b, Point *out, int size_a, int size_b) { 
     // n1 -> number of points in polygon a 
     // n2 -> number of points in polygon b 
-    int n1 = a.size(), n2 = b.size(); 
+    int n1 = size_a, n2 = size_b; 
   
     int ia = 0, ib = 0; 
     for (int i=1; i<n1; i++) 
@@ -87,14 +85,12 @@ vector<Point> merger(vector<Point > a,
     // finding the upper tangent 
     int inda = ia, indb = ib; 
     bool done = 0; 
-    while (!done) 
-    { 
+    while (!done) { 
         done = 1; 
         while (orientation(b[indb], a[inda], a[(inda+1)%n1]) >=0) 
             inda = (inda + 1) % n1; 
   
-        while (orientation(a[inda], b[indb], b[(n2+indb-1)%n2]) <=0) 
-        { 
+        while (orientation(a[inda], b[indb], b[(n2+indb-1)%n2]) <=0) { 
             indb = (n2+indb-1)%n2; 
             done = 0; 
         } 
@@ -118,32 +114,32 @@ vector<Point> merger(vector<Point > a,
     } 
   
     int lowera = inda, lowerb = indb; 
-    vector<Point> ret; 
-  
+ 
     //ret contains the convex hull after merging the two convex hulls 
     //with the points sorted in anti-clockwise order 
-    int ind = uppera; 
-    ret.push_back(a[uppera]); 
+    int ind = uppera;
+	int out_size=0; 
+    out[out_size++]=a[uppera]; 
     while (ind != lowera) 
     { 
         ind = (ind+1)%n1; 
-        ret.push_back(a[ind]); 
+        out[out_size++]=a[ind]; 
     } 
   
     ind = lowerb; 
-    ret.push_back(b[lowerb]); 
+    out[out_size++]=b[lowerb]; 
     while (ind != upperb) 
     { 
         ind = (ind+1)%n2; 
-        ret.push_back(b[ind]); 
+        out[out_size++]=b[ind]; 
     } 
-    return ret; 
+    return out_size; 
   
 } 
   
-vector<Point> convex_hull(vector<Point> &points){
+int convex_hull(Point *points,Point *out, int size){
    int ymin = points[0].y, min = 0; 
-   int n = points.size();	
+   int n = size;	
    for (int i = 1; i < n; i++) { 
      int y = points[i].y; 
      if ((y < ymin) || (ymin == y && 
@@ -164,18 +160,18 @@ vector<Point> convex_hull(vector<Point> &points){
        m++; 
    } 
 //	cout << "afrer eql points:\n";	
-   if (m < 3) return points; 
-   vector<Point> S; 
-   S.push_back(points[0]); 
-   S.push_back(points[1]); 
-   S.push_back(points[2]);  
+   if (m < 3) return 0; 
+   int out_size = 0; 
+   out[out_size++] =points[0]; 
+   out[out_size++] =points[1];
+   out[out_size++] =points[2]; 
    for (int i = 3; i < m; i++) { 
-      while (orientation(next_to_top(S), S.back(), points[i]) != -1) 
-         S.pop_back(); 
-      S.push_back(points[i]); 
+      while (orientation(out[out_size-1], out[out_size], points[i]) != -1) 
+         out_size--;//S.pop_back(); 
+      out[out_size++]=points[i]; 
    } 
 //	cout << "after main while:\n";	
-   return S;
+   return out_size;
    //while (!S.empty()) { 
    //    Point p = S.back();
    //    cout << "(" << p.x << ", " << p.y <<")" << endl; 
@@ -184,31 +180,29 @@ vector<Point> convex_hull(vector<Point> &points){
 } 
 
 // Returns the convex hull for the given set of points 
-vector<Point> divide(vector<Point> a) 
-{   
-	f++;
-    // If the number of points is less than 6 then the 
-    // function uses the brute algorithm to find the 
-    // convex hull 
-	cout << "begin"<<f<<"\n";	
-    if (a.size() <= 50) 
-        return convex_hull(a); 
-  
-    // left contains the left half points 
-    // right contains the right half points 
-    vector<Point>left, right; 
-    for (int i=0; i<a.size()/2; i++) 
-        left.push_back(a[i]); 
-    for (int i=a.size()/2; i<a.size(); i++) 
-        right.push_back(a[i]); 
-    cout << "divide"<<f<<"\n";
-    // convex hull for the left and right sets 
-    vector<Point>left_hull = divide(left); 
-    vector<Point>right_hull = divide(right); 
-    cout << "before merge"<<f<<"\n";
-    // merging the convex hulls 
-    return merger(left_hull, right_hull); 
-} 
+//vector<Point> divide( vector<Point> &a) 
+//{   
+//    // If the number of points is less than 6 then the 
+//    // function uses the brute algorithm to find the 
+//    // convex hull 
+//    if (a.size() <= 50) 
+//        return convex_hull(a); 
+//  
+//    // left contains the left half points 
+//    // right contains the right half points 
+//    vector<Point>left, right; 
+//    for (int i=0; i<a.size()/2; i++) 
+//        left.push_back(a[i]); 
+//    for (int i=a.size()/2; i<a.size(); i++) 
+//        right.push_back(a[i]); 
+//    cout << "divide"<<f<<"\n";
+//    // convex hull for the left and right sets 
+//    vector<Point>left_hull = divide(left); 
+//    vector<Point>right_hull = divide(right); 
+//    cout << "before merge"<<f<<"\n";
+//    // merging the convex hulls 
+//    return merger(left_hull, right_hull); 
+//} 
 
 int compare_X(const void *vp1, const void *vp2) { 
     Point *p1 = (Point *)vp1; 
@@ -216,24 +210,29 @@ int compare_X(const void *vp1, const void *vp2) {
     return ( p1->x - p2->x );
 }
 
-vector<Point> init_map(int n,int u_bound,int l_bound){
-	vector<Point> res;
+void init_map(Point * res,int n,int u_bound,int l_bound){
 	Point p;
 	for (int i = 0; i < n; i++){
 		p.y = 1+rand()%(u_bound-1);
 		p.x = 1+rand()%(l_bound-1);
-		res.push_back(p);
+		res[i] = p;
 	}
-	return res;
 }
 
 
 int main(int argc, char*argv[]) {
 	srand(static_cast<int>(time(0)));
 	int num_p = 100;
-	int proc_num, proc_id, flag;
+	int proc_num, proc_id, flag, sub_num_p, seq_res_size, sub_res_size;
 	double s_time_start = 0.0, p_time_start = 0.0;
     double s_time_finish = 0.0, p_time_finish = 0.0;
+	Point *points, *seq_res, *par_res, *sub_points, *sub_res, *points2;
+	Point for_disp;
+	MPI_Datatype PNT;
+    MPI_Datatype type[2] = { MPI_INT, MPI_INT };
+	MPI_Aint disp[2];
+	MPI_Aint addresses[3];
+    int blocklen[2] = { 1, 1 };
 	if (argc > 1) {
         num_p = atoi(argv[1]);
 	}
@@ -243,18 +242,47 @@ int main(int argc, char*argv[]) {
         std::cout << "Init MPI Error";
         return 0;
     }
+	MPI_Address(&(for_disp), &addresses[0]);
+    MPI_Address(&(for_disp.x), &addresses[1]);
+    MPI_Address(&(for_disp.y), &addresses[2]);
+    disp[0] = addresses[1] - addresses[0];
+    disp[1] = addresses[2] - addresses[0];
 	
-    vector<Point> a = init_map(n,n,n); 
-  	//MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
-    //MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
+	MPI_Type_create_struct(2, blocklen, disp, type, &PNT);
+    MPI_Type_commit(&PNT); 
+  	MPI_Comm_size(MPI_COMM_WORLD, &proc_num);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
 	//sub_num_p = static_cast<int>(ceil(static_cast<double>(num_p)/
     //                                    (static_cast<double>(proc_num))));
-    // sorting the set of points according 
-    // to the x-coordinate 
-    qsort(&a[0], a.size()	, sizeof(Point), compare_X);
-    vector<Point >ans = divide(a); 
-  	vector<Point >ans2 = convex_hull(a); 
-    cout << "convex hull:\n"; 
+	
+	//int hull_sizes[proc_num];
+	sub_num_p = num_p/proc_num;
+	sub_points = new Point[sub_num_p];
+	sub_res = new Point[sub_num_p];
+	MPI_Barrier(MPI_COMM_WORLD);
+	if (proc_id == 0) {
+		points = new Point[num_p];
+		points2 = new Point[num_p];
+		seq_res = new Point[num_p];
+		init_map(points,num_p,num_p,num_p);
+		for (int i = 0; i < num_p; i++)
+		points2[i] = points[i];
+		s_time_start = MPI_Wtime();
+    	qsort(points2, num_p, sizeof(Point), compare_X);
+  		seq_res_size = convex_hull(points, seq_res, num_p); 
+		s_time_finish = MPI_Wtime();
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+    p_time_start = MPI_Wtime();
+	MPI_Scatter(points2 , sub_num_p, PNT, sub_points,
+	sub_num_p, PNT, 0, MPI_COMM_WORLD);
+	cout<<proc_id<<endl;
+	for (int i = 0; i < sub_num_p;  i++) 
+      cout << sub_points[i].x << " "
+           << sub_points[i].y << endl; 
+    cout<<endl;
+	sub_res_size = convex_hull(sub_points,sub_res,sub_num_p);
+	MPI_Finalize();
    //for (auto e:ans) 
    //   cout << e.x << " "
    //        << e.y << endl; 
